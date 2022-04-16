b8922d15b265c60496c6ef8ba3a1e30704a86a3a
Алекс Киасса <chiazza73@gmail.com>
2022-04-17 00:17:59 +0300

    commit 3

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,15 +11,18 @@ class PageController extends Controller
       //  $flights = new flights();
        // $flights->Destination = 'tokyo';
        // $flights->save();
-        flight::create(['Destination' => 'Tokyo', 'DepartureDates' => '2021-05-20 13:55:44', 'Prices' => '20 000р']);
-        flight::create(['Destination' => 'London', 'DepartureDates' => '2021-06-26 05:00:00', 'Prices' => '35 000р']);
-        flight::create(['Destination' => 'Paris', 'DepartureDates' => '2021-09-11 07:30:00', 'Prices' => '15 000р']);
-        flight::create(['Destination' => 'Canada', 'DepartureDates' => '2021-07-20 01:35:00', 'Prices' => '28 999р']);
-        flight::create(['Destination' => 'USA', 'DepartureDates' => '2021-06-05 15:23:00', 'Prices' => '39 999р']);
-        flight::create(['Destination' => 'Sydney', 'DepartureDates' => '2021-12-05 09:00:00', 'Prices' => '23 889р']);
-        flight::create(['Destination' => 'London', 'DepartureDates' => '2021-08-26 10:00:00', 'Prices' => '20 999р']);
-        flight::create(['Destination' => 'London', 'DepartureDates' => '2021-06-01 18:00:00', 'Prices' => '25 999р']);
-        flight::create(['Destination' => 'Riga', 'DepartureDates' => '2021-07-01 22:00:00', 'Prices' => '12 999р']);
+        /*
+         * Вносит заготовленные записи в таблицу flight
+         */
+        flight::create(['Destination' => 'Tokyo', 'DepartureDates' => '2022-05-20 13:55:44', 'Prices' => '20 000р']);
+        flight::create(['Destination' => 'London', 'DepartureDates' => '2022-06-26 05:00:00', 'Prices' => '35 000р']);
+        flight::create(['Destination' => 'Paris', 'DepartureDates' => '2022-09-11 07:30:00', 'Prices' => '15 000р']);
+        flight::create(['Destination' => 'Canada', 'DepartureDates' => '2022-07-20 01:35:00', 'Prices' => '28 999р']);
+        flight::create(['Destination' => 'USA', 'DepartureDates' => '2022-06-05 15:23:00', 'Prices' => '39 999р']);
+        flight::create(['Destination' => 'Sydney', 'DepartureDates' => '2023-12-05 09:00:00', 'Prices' => '23 889р']);
+        flight::create(['Destination' => 'London', 'DepartureDates' => '2023-08-26 10:00:00', 'Prices' => '20 999р']);
+        flight::create(['Destination' => 'London', 'DepartureDates' => '2022-06-01 18:00:00', 'Prices' => '25 999р']);
+        flight::create(['Destination' => 'Riga', 'DepartureDates' => '2023-07-01 22:00:00', 'Prices' => '12 999р']);
 
         return response()->json(true);
 
